6fad19b28f9b9e8d74f5c20a45f28019396abe98
DTB <183888664+DoTheBetter@users.noreply.github.com>
2025-04-14 18:26:44 +0800

    ax6000

---
+++ padavanonly/diy-part2-mt7986-ax6000.sh
@@ -0,0 +1,191 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+function config_del(){
+    yes="CONFIG_$1=y"
+    no="# CONFIG_$1 is not set"
+
+    sed -i "s/$yes/$no/" .config
+}
+
+function config_add(){
+    yes="CONFIG_$1=y"
+    no="# CONFIG_$1 is not set"
+
+    sed -i "s/${no}/${yes}/" .config
+
+    if ! grep -q "$yes" .config; then
+        echo "$yes" >> .config
+    fi
+}
+
+function config_package_del(){
+    package="PACKAGE_$1"
+    config_del $package
+}
+
+function config_package_add(){
+    package="PACKAGE_$1"
+    config_add $package
+}
+
+function drop_package(){
+    if [ "$1" != "golang" ];then
+        # feeds/base -> package
+        find package/ -follow -name $1 -not -path "package/custom/*" | xargs -rt rm -rf
+        find feeds/ -follow -name $1 -not -path "feeds/base/custom/*" | xargs -rt rm -rf
+    fi
+}
+function clean_packages(){
+    path=$1
+    dir=$(ls -l ${path} | awk '/^d/ {print $NF}')
+    for item in ${dir}
+        do
+            drop_package ${item}
+        done
+}
+
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+##########################
+#设置官方默认包https://downloads.immortalwrt.org/releases/24.10.0/targets/mediatek/filogic/profiles.json
+default_packages=(
+        "autocore",
+        "base-files",
+        "block-mount",
+        "bridger",
+        "ca-bundle",
+        "default-settings-chn",
+        "dnsmasq-full",
+        "dropbear",
+        "firewall4",
+        "fitblk",
+        "fstools",
+        "libc",
+        "libgcc",
+        "libustream-openssl",
+        "logd",
+        "luci-app-package-manager",
+        "luci-compat",
+        "luci-lib-base",
+        "luci-lib-ipkg",
+        "luci-light",
+        "mtd",
+        "netifd",
+        "nftables",
+        "odhcp6c",
+        "odhcpd-ipv6only",
+        "opkg",
+        "ppp",
+        "ppp-mod-pppoe",
+        "procd-ujail",
+        "uboot-envtools",
+        "uci",
+        "uclient-fetch",
+        "urandom-seed",
+        "urngd",
+        "wpad-openssl"
+)
+# 循环调用 config_package_add 函数
+for package in "${default_packages[@]}"; do
+    config_package_add "$package"
+done
+################################################################
+
+# 设置'root'密码为 'password'
+sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.10.10/g' package/base-files/files/bin/config_generate
+# 添加编译时间到版本信息
+sed -i "s/DISTRIB_DESCRIPTION='.*'/DISTRIB_DESCRIPTION='${REPO_NAME} ${OpenWrt_VERSION} ${OpenWrt_ARCH} Built on $(date +%Y%m%d)'/" package/base-files/files/etc/openwrt_release
+# 添加编译时间到 /etc/banner
+#echo "Build Time: $(date +%Y%m%d)" >> package/base-files/files/etc/banner
+
+# 删除
+# Sound Support
+config_package_del kmod-sound-core
+config_package_del kmod-ac97
+config_package_del kmod-sound-hda-core
+config_package_del kmod-sound-hda-codec-hdmi
+config_package_del kmod-sound-hda-codec-realtek
+config_package_del kmod-sound-hda-codec-via
+config_package_del kmod-sound-hda-intel
+config_package_del kmod-sound-i8x0
+config_package_del kmod-sound-mpu401
+config_package_del kmod-sound-via82xx
+config_package_del kmod-usb-audio
+
+# 新增
+# bbr
+config_package_add kmod-tcp-bbr
+# autocore + lm-sensors-detect： cpu 频率、温度
+config_package_add autocore
+config_package_add lm-sensors-detect
+# bash
+config_package_add bash
+# 更改默认 Shell 为 bash
+sed -i 's|/bin/ash|/bin/bash|g' package/base-files/files/etc/passwd
+# nano 替代 vim
+config_package_add nano
+# curl
+config_package_add curl
+# upnp
+#config_package_add luci-app-upnp
+# tty 终端
+config_package_add luci-app-ttyd
+# tty 免登录
+sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+# kms
+config_package_add luci-app-vlmcsd
+# socat端口映射
+config_package_add luci-app-socat
+# ipv6
+config_package_add ipv6helper
+# ddns-go
+config_package_add luci-app-ddns-go
+# openclash
+config_package_add luci-app-openclash
+# homeproxy
+config_package_add luci-app-homeproxy
+
+
+#### 第三方软件包
+mkdir -p package/custom
+git clone --depth 1 https://github.com/DoTheBetter/OpenWrt-Packages.git package/custom
+clean_packages package/custom
+
+# golang
+rm -rf feeds/packages/lang/golang
+mv package/custom/golang feeds/packages/lang/
+
+# argon 主题
+config_package_add luci-theme-argon
+# 修改默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+## 定时任务。重启、关机、重启网络、释放内存、系统清理、网络共享、关闭网络、自动检测断网重连、MWAN3负载均衡检测重连、自定义脚本等10多个功能
+config_package_add luci-app-autotimeset
+config_package_add luci-lib-ipkg
+## 分区扩容。一键自动格式化分区、扩容、自动挂载插件，专为OPENWRT设计，简化OPENWRT在分区挂载上烦锁的操作
+config_package_add luci-app-partexp
+#设置向导
+config_package_add luci-app-netwizard
+#网络速度测试
+config_package_add luci-app-netspeedtest
