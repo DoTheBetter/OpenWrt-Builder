53c68a05b8df311dd50be54edcf6dc63b5692a20
DTB <183888664+DoTheBetter@users.noreply.github.com>
2025-04-17 09:02:17 +0800

    ci(padavanonly): 更新工作流和脚本以优化构建过程

--- padavanonly/diy-part1-mt7986-ax6000.sh
+++ padavanonly/diy-part1-mt7986-ax6000.sh
@@ -16,3 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+#echo "src-git mtk_openwrt_feed https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds" >> feeds.conf.default
+#./scripts/feeds update -a
+#./scripts/feeds install -a
--- padavanonly/diy-part2-mt7986-ax6000.sh
+++ padavanonly/diy-part2-mt7986-ax6000.sh
@@ -65,6 +65,11 @@ function git_sparse_clone() {
 }
 
 ##########################
+config_del TARGET_MULTI_PROFILE
+config_del TARGET_DEVICE_mediatek_mt7986_DEVICE_glinet_gl-mt6000
+config_del TARGET_DEVICE_mediatek_mt7986_DEVICE_tplink_tl-xdr6086
+config_del TARGET_DEVICE_mediatek_mt7986_DEVICE_tplink_tl-xdr6088
+config_del TARGET_DEVICE_mediatek_mt7986_DEVICE_xiaomi_redmi-router-ax6000-stock
 
 # 设置'root'密码为 'password'
 sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
@@ -73,63 +78,55 @@ sed -i 's/192.168.1.1/192.168.10.10/g' package/base-files/files/bin/config_gener
 # 添加编译时间到版本信息
 sed -i "s/DISTRIB_DESCRIPTION='.*'/DISTRIB_DESCRIPTION='${REPO_NAME} ${OpenWrt_VERSION} ${OpenWrt_ARCH} Built on $(date +%Y%m%d)'/" package/base-files/files/etc/openwrt_release
 
-## 新增
-## bbr
-#config_package_add kmod-tcp-bbr
-## autocore + lm-sensors-detect： cpu 频率、温度
-#config_package_add autocore
-#config_package_add lm-sensors-detect
-## bash
-#config_package_add bash
-## 更改默认 Shell 为 bash
-#sed -i 's|/bin/ash|/bin/bash|g' package/base-files/files/etc/passwd
-## nano 替代 vim
-#config_package_add nano
-## curl
-#config_package_add curl
-## upnp
-#config_package_add luci-app-upnp
-## ipv6
-#config_package_add ipv6helper
-## tty 终端
-#config_package_add luci-app-ttyd
-## tty 免登录
-#sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
-#
-## kms
-#config_package_add luci-app-vlmcsd
-## socat端口映射
-#config_package_add luci-app-socat
-## ddns-go
-#config_package_add luci-app-ddns-go
-## openclash
-#config_package_add luci-app-openclash
-## homeproxy
-#config_package_add luci-app-homeproxy
-## smartdns
-#config_package_add luci-app-smartdns
-## adguardhome
-#config_package_add adguardhome
-#
-##### 第三方软件包
-#mkdir -p package/custom
-#git clone --depth 1 https://github.com/DoTheBetter/OpenWrt-Packages.git package/custom
-#clean_packages package/custom
-#
-## golang
-#rm -rf feeds/packages/lang/golang
-#mv package/custom/golang feeds/packages/lang/
-#
-## argon 主题
-#config_package_add luci-theme-argon
-## 修改默认主题
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-#
-### 定时任务。重启、关机、重启网络、释放内存、系统清理、网络共享、关闭网络、自动检测断网重连、MWAN3负载均衡检测重连、自定义脚本等10多个功能
-#config_package_add luci-app-autotimeset
-#config_package_add luci-lib-ipkg
-#
-##设置向导
-#config_package_add luci-app-netwizard
-##网络速度测试
-#config_package_add luci-app-netspeedtest
+# 新增
+# bbr
+config_package_add kmod-tcp-bbr
+# autocore + lm-sensors-detect： cpu 频率、温度
+config_package_add autocore-arm
+config_package_add lm-sensors-detect
+# bash
+config_package_add bash
+# 更改默认 Shell 为 bash
+sed -i 's|/bin/ash|/bin/bash|g' package/base-files/files/etc/passwd
+# nano 替代 vim
+config_package_add nano
+# curl
+config_package_add curl
+# tty 终端
+config_package_add luci-app-ttyd
+# tty 免登录
+sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+
+# kms
+config_package_add luci-app-vlmcsd
+# homeproxy
+config_package_add luci-app-homeproxy
+# smartdns
+config_package_add luci-app-smartdns
+# adguardhome
+config_package_add adguardhome
+
+#### 第三方软件包
+git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
+config_package_add luci-app-lucky
+
+git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/nikki
+config_package_add luci-app-nikki
+
+
+mkdir -p package/custom
+git clone --depth 1 https://github.com/DoTheBetter/OpenWrt-Packages.git package/custom
+clean_packages package/custom
+# golang
+rm -rf feeds/packages/lang/golang
+mv package/custom/golang feeds/packages/lang/
+# argon 主题
+config_package_add luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+## 定时任务。重启、关机、重启网络、释放内存、系统清理、网络共享、关闭网络、自动检测断网重连、MWAN3负载均衡检测重连、自定义脚本等10多个功能
+config_package_add luci-app-autotimeset
+config_package_add luci-lib-ipkg
+#设置向导
+config_package_add luci-app-netwizard
+#网络速度测试
+config_package_add luci-app-netspeedtest
